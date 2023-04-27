6df6919708731de1db870777f8be7a2c10509ce5
gabri <gabrieuvelezmoro@gmail.com>
2023-04-27 10:20:28 -0300

    style: table

---
+++ src/components/table/styles.ts
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+export const StyledTable = styled.table`
+    width: 100%;
+    border-collapse: collapse;
+    text-align: center;
+    overflow: hidden;
+    background-color: #1C1C1C;
+`
+
+export const StyledTr = styled.tr`
+  
+`
+
+export const StyledTh = styled.th`
+    font-weight: normal;
+    padding: 10px;
+    text-transform: capitalize;
+    font-weight: 600;
+    font-size: 14px;
+
+`
+
+export const StyledTd = styled.td`
+    border-width: 3px 0;
+    border-style: solid;
+    color: #4F4F4F;
+    padding: 5px 15px;
+`
